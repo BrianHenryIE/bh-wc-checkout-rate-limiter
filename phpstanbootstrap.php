4a4a73a5ba239c79409a04cbc3ead79d4039e3e5
Brian Henry <BrianHenryIE@gmail.com>
2022-05-13 11:39:39 -0700

    Create phpstanbootstrap.php

---
+++ phpstanbootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Define constants that PhpStan cannot find.
+ *
+ * @see https://phpstan.org/user-guide/discovering-symbols#global-constants
+ *
+ * @package brianhenryie/bh-wc-checkout-rate-limiter
+ */
+
+define( 'WP_CONTENT_DIR', __DIR__ . '/wp-content' );
+define( 'WP_PLUGIN_DIR', __DIR__ . '/wp-content/plugins' );
