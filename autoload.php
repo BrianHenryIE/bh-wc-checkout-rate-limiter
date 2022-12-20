ce6ef4459f4cc5d0487d12b5a91f2442699742db
Brian Henry <BrianHenryIE@gmail.com>
2022-12-20 07:16:11 -0800

    Use Alley Interactive autoloader
    And some composer updates

--- autoload.php
+++ autoload.php
@@ -4,39 +4,20 @@
  *
  * Uses classmap, PSR4 & wp-namespace-autoloader.
  *
- * @link              https://BrianHenryIE.com
+ * @link              http://example.com
  * @since             1.0.0
- * @package           BH_WC_Checkout_Rate_Limiter
+ * @package           brianhenryie/bh-wc-checkout-rate-limiter
  *
  * @see https://github.com/pablo-sg-pacheco/wp-namespace-autoloader/
  */
 
 namespace BrianHenryIE\Checkout_Rate_Limiter;
 
-use BrianHenryIE\Checkout_Rate_Limiter\Pablo_Pacheco\WP_Namespace_Autoloader\WP_Namespace_Autoloader;
-
-$class_map_files = array(
-	__DIR__ . '/autoload-classmap.php',
-);
-foreach ( $class_map_files as $class_map_file ) {
-	if ( file_exists( $class_map_file ) ) {
-
-		$class_map = include $class_map_file;
-
-		if ( is_array( $class_map ) ) {
-			spl_autoload_register(
-				function ( $classname ) use ( $class_map ) {
-
-					if ( array_key_exists( $classname, $class_map ) && file_exists( $class_map[ $classname ] ) ) {
-						require_once $class_map[ $classname ];
-					}
-				}
-			);
-		}
-	}
-}
+use BrianHenryIE\Checkout_Rate_Limiter\Alley_Interactive\Autoloader\Autoloader;
 
 require_once __DIR__ . '/vendor-prefixed/autoload.php';
 
-$wpcs_autoloader = new WP_Namespace_Autoloader( array( 'classes_dir' => array( 'src' ) ) );
-$wpcs_autoloader->init();
+Autoloader::generate(
+	'BrianHenryIE\Checkout_Rate_Limiter',
+	__DIR__ . '/src',
+)->register();
